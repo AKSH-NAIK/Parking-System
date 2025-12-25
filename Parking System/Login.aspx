0dc6fe825e96afb0ce4ce124fe05182f761e37d1
Aksh Naik <akshnaik1907@gmail.com>
2025-12-25 19:38:19 +0530

    Created Login.aspx

--- Parking System/Login.aspx
+++ Parking System/Login.aspx
@@ -9,6 +9,23 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            Staff ID:
+<asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+<asp:RequiredFieldValidator ID="rfvName" runat="server"
+ControlToValidate="txtName"
+ErrorMessage="Name is required"
+ForeColor="Red">
+</asp:RequiredFieldValidator>
+<br /><br />
+            Password:
+<asp:TextBox ID="txtPassword" runat="server"
+TextMode="Password"></asp:TextBox>
+<asp:RequiredFieldValidator ID="rfvPassword" runat="server"
+ControlToValidate="txtPassword"
+ErrorMessage="Password is required"
+ForeColor="Red">
+</asp:RequiredFieldValidator>
+<br /><br />
         </div>
     </form>
 </body>
--- Parking System/Login.aspx.designer.cs
+++ Parking System/Login.aspx.designer.cs
@@ -22,5 +22,41 @@ namespace Parking_System
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// txtName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtName;
+
+        /// <summary>
+        /// rfvName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvName;
+
+        /// <summary>
+        /// txtPassword control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPassword;
+
+        /// <summary>
+        /// rfvPassword control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvPassword;
     }
 }
